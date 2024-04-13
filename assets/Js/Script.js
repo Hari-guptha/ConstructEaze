bf892d622b50a1ab1e8c41408cfaa7a6fc8b34a0
Hari_Guptha <80567787+Hari-guptha@users.noreply.github.com>
2024-04-13 18:25:44 +0530

    responsive menu

--- assets/Js/Script.js
+++ assets/Js/Script.js
@@ -12,3 +12,20 @@ const ClosePopUp = () => {
     pop.style.display = "none";
     blur.style.display = "none";
 }
+
+
+const tiggermenu = () => {
+    const pop = document.getElementById('phoneSidebar');
+    const blur = document.getElementById('backgroundBlur');
+    pop.style.display = "block";
+    blur.style.display = "block";
+}
+
+
+const Closemenu = () => {
+    const pop = document.getElementById('phoneSidebar');
+    const blur = document.getElementById('backgroundBlur');
+    pop.style.display = "none";
+    blur.style.display = "none";
+}
+
