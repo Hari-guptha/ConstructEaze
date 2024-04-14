2e1ce30fe5da31783be89d82a651dc0acc024c7c
Hari_Guptha <80567787+Hari-guptha@users.noreply.github.com>
2024-04-15 02:39:20 +0530

    po

--- assets/Js/Script.js
+++ assets/Js/Script.js
@@ -29,3 +29,42 @@ const Closemenu = () => {
     blur.style.display = "none";
 }
 
+
+
+const tokenSet = new Set(); // Set to store generated tokens
+
+// Unique Token Generator Function
+function generateUniqueToken() {
+    const tokenLength = 10; // Set the desired length of your token
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let token;
+
+    // Generate a unique token
+    do {
+        token = ''; 
+        for (let i = 0; i < tokenLength; i++) {
+            const randomIndex = Math.floor(Math.random() * characters.length);
+            token += characters.charAt(randomIndex);
+        }
+    } while (tokenSet.has(token)); // Check if the token already exists
+
+    tokenSet.add(token); // Add the generated token to the set
+
+    return token;
+}
+
+document.getElementById("OrderId").innerText = '#' + generateUniqueToken();
+
+
+
+
+function getCurrentDate() {
+    var currentDate = new Date();
+    var year = currentDate.getFullYear();
+    var month = ('0' + (currentDate.getMonth() + 1)).slice(-2); // Adding 1 because getMonth() returns zero-based index (0-11)
+    var day = ('0' + currentDate.getDate()).slice(-2);
+
+    return year + "-" + month + "-" + day;
+}
+
+document.getElementById("OrderDate").innerText = getCurrentDate();
